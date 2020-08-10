e84ea636179beff336a0074ab7c915ac06c7f47a
Charlie <charles.j.montoya@gmail.com>
2020-08-09 21:16:46 -0400

    adding linked list

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -6,6 +6,44 @@ class HashTableEntry:
         self.key = key
         self.value = value
         self.next = None
+    
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next
+
+    def set_next(self, new_next):
+        self.next = new_next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add_to_head(self, value):
+        node = HashTableEntry(key, value)
+
+        if self.head is not None:
+            node.set_next(self.head)
+
+        self.head = node
+
+    def contains(self, value):
+        if not self.head:
+            return False
+
+        current = self.head
+
+        while current:
+            if current.get_value() == value:
+                return True
+
+            current = current.get_next()
+
+        return False
+
+    
 
 
 # Hash table can't have fewer than this many slots
